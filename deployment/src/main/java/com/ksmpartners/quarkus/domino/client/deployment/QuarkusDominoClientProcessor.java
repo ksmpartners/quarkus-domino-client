297cce517d0b4f30f85da029cf69613abd1bdb22
melloware <mellowaredev@gmail.com>
2024-02-03 07:57:23 -0500

    5.9.1.0

--- deployment/src/main/java/com/ksmpartners/quarkus/domino/client/deployment/QuarkusDominoClientProcessor.java
+++ deployment/src/main/java/com/ksmpartners/quarkus/domino/client/deployment/QuarkusDominoClientProcessor.java
@@ -1,6 +1,9 @@
 package com.ksmpartners.quarkus.domino.client.deployment;
 
-import com.dominodatalab.api.model.DominoVersion;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
@@ -9,10 +12,6 @@ import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 class QuarkusDominoClientProcessor {
 
@@ -33,7 +32,9 @@ class QuarkusDominoClientProcessor {
     void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
                                CombinedIndexBuildItem combinedIndex) {
         // All Model Objects
-        final List<String> classNames = new ArrayList<>(collectClassesInPackage(combinedIndex, DominoVersion.class.getPackageName()));
+        final List<String> classNames = new ArrayList<>();
+        classNames.addAll(collectClassesInPackage(combinedIndex, "com.dominodatalab.api.model"));
+        classNames.addAll(collectClassesInPackage(combinedIndex, "com.dominodatalab.pub.model"));
 
         reflectiveClass.produce(ReflectiveClassBuildItem.builder(classNames.toArray(new String[0])).methods(true).build());
     }
